5f3a27c0f046c41ebe886d6136fe9130ff3093ca
Iwada Bassey <basmo13@yahoo.co.uk>
2011-11-28 14:11:28 -0800

    CourseX Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coursex::Application.config.secret_token = '8f24013114b566048748b62b86d58a5e709a8e2027424af9f1f7291a560fd0c123f724b1f374caebbc5aae1c65b285df9411bee0646dcdcc96cf909a510688ba'
