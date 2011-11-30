853205fe57f722a1958239047d98e75935f8d07d
Iwada Bassey <basmo13@yahoo.co.uk>
2011-11-29 17:31:51 -0800

    Login pages Added

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.9'
+gem 'sorcery'
+
+gem 'haml'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Coursex::Application.routes.draw do
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
+
+  resources :users
+  resources :sessions
+
+
+  get "sessions/new"
+
+  get "users/new"
+
   get "pages/home"
 
   get "pages/contact"
